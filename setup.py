35f5582b13ee98a3080720f32d9a5430da78e1bb
Haloroute <quangtuyensctn@gmail.com>
2025-06-19 21:26:25 +0700

    feat: update package's info

--- setup.py
+++ setup.py
@@ -2,12 +2,25 @@ from setuptools import setup, find_packages
 
 setup(
     name='taming-transformers',
-    version='0.0.2',
+    version='0.0.1',
     description='Taming Transformers for High-Resolution Image Synthesis',
     packages=find_packages(),
     install_requires=[
         'torch',
+        'torchvision',
+        'torchaudio',
         'numpy',
-        'tqdm',
+        'albumentations',
+        'opencv-python',
+        'pudb',
+        'imageio',
+        'imageio-ffmpeg',
+        'pytorch-lightning',
+        'omegaconf',
+        'test-tube',
+        'streamlit',
+        'einops',
+        'more-itertools',
+        'transformers',
     ],
 )
